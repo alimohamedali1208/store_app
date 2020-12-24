bb9e79edf0f0167586fd38186f0a40eab1ee21af
alimohamedali1208 <alimohamedali1208@gmail.com>
2020-12-24 16:27:59 +0200

    test

---
+++ lib/components/Product.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Product extends StatefulWidget {
+  @override
+  _ProductState createState() => _ProductState();
+}
+
+class _ProductState extends State<Product> {
+  var product_list=[
+    {
+      // i luv Ammar
+    }
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
