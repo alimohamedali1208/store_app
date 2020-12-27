6f1c27c7d59fe7a0ecc2f85a49ac9aee8fcfd45d
alimohamedali1208 <alimohamedali1208@gmail.com>
2020-12-27 17:59:55 +0200

    database working 3ndy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'components/Home.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     home: HomePage(),
 
